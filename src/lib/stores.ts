907ab77cf02b37ac947c3e78500e9122b3f432e6
ThatXliner <bryan.hu.2020@gmail.com>
2025-08-23 20:18:45 -0700

    Documented justification

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -5,9 +5,18 @@ import { getActiveAnnotation } from "./editor/plugins/annotations/utils";
 import type { Annotation, Annotations } from "./editor/plugins/annotations";
 
 export const editorView = writable<EditorView>();
-// TODO: Document justification
+// We need to manually hook into when the annotations change
+// (editorView never gets updated... maybe I could do that? Would
+// that be premature optimization or just passing in a reference?)
+// So we manually manage and sync our own version of annotations
+// as opposed to derived from editorView
 export const annotations = writable<Annotations | undefined>();
+// For similar reasons (getActiveAnnotation relies on editor.state, which relies on editorView)
+// we have to manually manage and sync our own version of activeComment
 export const activeComment = writable<Annotation<"comment"> | undefined>();
+
+// In case we decide to bite the dust with updating editorView every time,
+// here is some code to do that:
 // export const activeComment = derived(editorView, ($editorView) => {
 //   if (!$editorView) return undefined;
 //   return getActiveAnnotation($editorView.state, "comment");
