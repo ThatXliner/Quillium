d679d8e8f74d4b41ab2429f88c02130c23c72d09
ThatXliner <bryan.hu.2020@gmail.com>
2025-08-31 15:01:27 -0700

    Slightly better AI Sidebar UI

--- src/lib/ai/AISidebar.svelte
+++ src/lib/ai/AISidebar.svelte
@@ -1,23 +1,31 @@
 <script lang="ts">
-// or typescript enum?
-type Action = null | "feedback" | "revise";
-let action = $state<Action>(null);
-async function provideFeedback() {
-	action = "feedback";
-}
-async function reword() {}
+    // or typescript enum?
+    type Action = null | "feedback" | "revise";
+    let action = $state<Action>(null);
+    async function provideFeedback() {
+        action = "feedback";
+    }
+    async function reword() {}
 </script>
+
 <!-- actions -->
-<div class="h-full flex justify-center">
-    {#if action === null}
-    <button onclick={provideFeedback}>Provide feedback</button>
-    <button onclick={reword}>Revise/reword for ___ (flow? conciseness? both?)</button>
-    <!-- Settings config for configuring
+<div class="w-[300px] h-screen flex flex-col justify-center p-3">
+    <!-- TODO: a nice looking gradient -->
+    <div
+        class="backdrop-blur-md rounded-lg p-5 bg-slate-400/70 border border-white/30 shadow-lg"
+    >
+        {#if action === null}
+            <button onclick={provideFeedback}>Provide feedback</button>
+            <button onclick={reword}
+                >Revise/reword for ___ (flow? conciseness? both?)</button
+            >
+            <!-- Settings config for configuring
    - goals
   - essay prompt
  - tone, style
 - preference of revision (word count? conciseness or for flow or both) -->
-{:else if action === 'feedback'}
-    <p>Loading...</p>
-{/if}
+        {:else if action === "feedback"}
+            <p>Loading...</p>
+        {/if}
+    </div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,17 @@
 <script>
-import Annotations from "$lib/editor/plugins/annotations/Annotations.svelte";
-import Editor from "$lib/editor/Editor.svelte";
+    import Annotations from "$lib/editor/plugins/annotations/Annotations.svelte";
+    import Editor from "$lib/editor/Editor.svelte";
+    import AiSidebar from "$lib/ai/AISidebar.svelte";
 </script>
 
 <div class="flex">
-  <Editor />
-  <Annotations />
+    <AiSidebar />
+    <Editor />
+    <Annotations />
 </div>
 
 <style>
-  :global(html) {
-    background-color: lightgray;
-  }
+    :global(html) {
+        background-color: lightgray;
+    }
 </style>
