e684d1ee35deaacc577cd8624e737bc2263635a1
ThatXliner <bryan.hu.2020@gmail.com>
2026-02-19 15:27:17 -0800

    Fix multiple instances of codemirror state

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,12 @@ const host = process.env.TAURI_DEV_HOST;
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
 	plugins: [sveltekit(), tailwindcss()],
+	resolve: {
+		dedupe: [
+			"@codemirror/state",
+			"@codemirror/view",
+		],
+	},
 
 	// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
 	//
