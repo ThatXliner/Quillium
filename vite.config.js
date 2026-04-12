b50ea6d52e5f8918a7bf6b93c3f26546a3cc21ea
ThatXliner <bryan.hu.2020@gmail.com>
2026-04-12 00:25:50 -0700

    delete unnecessary (?) posthog alias

--- vite.config.js
+++ vite.config.js
@@ -13,9 +13,6 @@ export default defineConfig(async () => ({
         __APP_VERSION__: JSON.stringify(pkg.version),
     },
     resolve: {
-        alias: {
-            "posthog-js": "posthog-js/dist/module.full.no-external",
-        },
         dedupe: ["@codemirror/state", "@codemirror/view"],
     },
 
